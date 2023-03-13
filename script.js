6cdd447e08f6097467fe22ddb29569494208542b
Hajjioui-MH <hajjioui.mh@gmail.com>
2023-03-13 20:23:04 +0100

    page login

---
+++ script.js
@@ -0,0 +1,7 @@
+// const emailValue = document.querySelector('#mail').value;
+// const passwordValue = document.querySelector('#password').value;
+
+const loginButton = document.querySelector('.login-btn');
+loginButton.addEventListener('click', function login() {
+    console.log('emailValue==> ', emailValue)
+})
